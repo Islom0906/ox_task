721beff117be3ccd848b76985f5e4e7b4e35027a
Islom0906 <iabdugofurov99@gmail.com>
2023-04-02 01:27:07 +0500

    remove slice

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,6 @@ import {configureStore} from "@reduxjs/toolkit";
 import ProductSlice from "../slice/productSlice";
 
 export default configureStore({
-    reducer:{
-        product:ProductSlice
-    },
+    reducer:{},
     devTools: process.env.NODE_ENV !== 'production',
 })
